6b6f8a61417c7c3a793ac8047548df02d46d92bf
sonhk <sonhk@irtech.com.vn>
2022-03-30 09:22:49 +0700

    U: Done SS5

--- Module 2/src/_5_access_modifier/practice/p2/Car.java
+++ Module 2/src/_5_access_modifier/practice/p2/Car.java
@@ -1,4 +1,17 @@
 package _5_access_modifier.practice.p2;
 
 public class Car {
+
+    private String name;
+
+    private String engine;
+
+    public static int numberOfCars;
+
+    public Car(String name, String engine) {
+        this.name = name;
+        this.engine = engine;
+        numberOfCars++;
+    }
+
 }
