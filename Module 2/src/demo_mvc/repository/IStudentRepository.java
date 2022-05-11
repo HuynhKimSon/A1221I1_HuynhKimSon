b7ef0067202eb2285768e4fe4bf0d8a11fabed2c
Huynh Kim Son <hkssqdc@gmail.com>
2022-05-11 20:12:29 +0700

    U

--- Module 2/src/demo_mvc/repository/IStudentRepository.java
+++
@@ -1,7 +0,0 @@
-package demo_mvc.repository;
-
-import demo_mvc.model.Student;
-
-public interface IStudentRepository {
-    void saveStudent(Student student);
-}
--- Module 2/src/demo_mvc/service/IStudentService.java
+++
@@ -1,7 +0,0 @@
-package demo_mvc.service;
-
-import demo_mvc.model.Student;
-
-public interface IStudentService {
-    void addStudent(Student student);
-}
