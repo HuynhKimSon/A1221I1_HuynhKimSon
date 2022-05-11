0fec8d579f8c75325818fda49bab316f1ea83e0f
Huynh Kim Son <hkssqdc@gmail.com>
2022-05-11 20:10:07 +0700

    U: code

---
+++ Final/Final_Module_2/src/Bank/util/ConstantUtil.java
@@ -0,0 +1,7 @@
+package Bank.util;
+
+public interface ConstantUtil {
+    interface PATH {
+        String BANK_ACCOUNT = "src/Bank/data/bank_account.csv";
+    }
+}
--- product_demo/src/util/ConstantUtil.java
+++
@@ -1,5 +0,0 @@
-package util;
-
-public interface ConstantUtil {
-    String path = "src/data/product.csv";
-}
